4d2042aa909cf7db4f4a77f9f3b928242a96de0c
Kleadron <kleadron04@gmail.com>
2025-09-21 18:45:57 -0700

    Replace BufferPools fix with a less yucky version

--- BEPUutilities/ResourceManagement/BufferPools.cs
+++ BEPUutilities/ResourceManagement/BufferPools.cs
@@ -6,7 +6,6 @@ using System.Threading;
 
 namespace BEPUutilities.ResourceManagement
 {
-#if XBOX360
     /// <summary>
     /// Contains locking and thread static buffer pools for the specified type.
     /// </summary>
@@ -17,61 +16,18 @@ namespace BEPUutilities.ResourceManagement
         /// Gets a buffer pool for this type which provides thread safe resource acquisition and return.</summary>
         public static LockingBufferPool<T> Locking { get; private set; }
 
-        // NOTE: THIS SHIT BROKEN ON XBOX :(
+        // NOTE: THIS IS BROKEN ON XBOX :(
         //[ThreadStatic]
         //private static BufferPool<T> threadPool;
-        static Dictionary<Thread, BufferPool<T>> threadPools;
 
         /// <summary>
         /// Gets the pool associated with this thread.
         /// </summary>
         public static BufferPool<T> Thread
         {
+            // ThreadStatic doesn't work on Xbox, so I'm returning a reference to the Locking pool instead.
             //get { return threadPool ?? (threadPool = new BufferPool<T>()); }
-            get
-            {
-                BufferPool<T> pool;
-                lock (threadPools)
-                {
-                    Thread currentThread = System.Threading.Thread.CurrentThread; // this sucks
-
-                    if (!threadPools.TryGetValue(currentThread, out pool))
-                    {
-                        pool = new BufferPool<T>();
-                        threadPools[currentThread] = pool;
-                    }
-                }
-                return pool;
-            }
-        }
-
-        static BufferPools()
-        {
-            threadPools = new Dictionary<Thread, BufferPool<T>>();
-            Locking = new LockingBufferPool<T>();
-        }
-    }
-#else
-    /// <summary>
-    /// Contains locking and thread static buffer pools for the specified type.
-    /// </summary>
-    /// <typeparam name="T">Type of element in the buffers stored in the pools.</typeparam>
-    public static class BufferPools<T>
-    {
-        /// <summary>
-        /// Gets a buffer pool for this type which provides thread safe resource acquisition and return.</summary>
-        public static LockingBufferPool<T> Locking { get; private set; }
-
-        // NOTE: THIS SHIT BROKEN ON XBOX :(
-        [ThreadStatic]
-        private static BufferPool<T> threadPool;
-
-        /// <summary>
-        /// Gets the pool associated with this thread.
-        /// </summary>
-        public static BufferPool<T> Thread
-        {
-            get { return threadPool ?? (threadPool = new BufferPool<T>()); }
+            get { return Locking; }
         }
 
         static BufferPools()
@@ -79,5 +35,4 @@ namespace BEPUutilities.ResourceManagement
             Locking = new LockingBufferPool<T>();
         }
     }
-#endif
 }
